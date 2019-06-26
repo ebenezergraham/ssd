afa89bbb813252c8eea5d2a89918bab854b03fde
ebenezergraham <egraham15@alustudent.com>
2019-06-26 19:04:09 +0400

    Using one object to run multiple threads

--- src/me/ebenezergraham/ssd/controllers/IndexServlet.java
+++ src/me/ebenezergraham/ssd/controllers/IndexServlet.java
@@ -7,32 +7,36 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet( name = "IndexServlet",urlPatterns = "/index")
 public class IndexServlet extends HttpServlet {
-	FileService fileService;
+	FileService fileService = new FileService();
 	@Override
 	protected void doGet(HttpServletRequest request,
 	                     HttpServletResponse response){
-		this.fileService = new FileService(request,response);
+		fileService.response = response;
+		fileService.request = request;
 		fileService.run();
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest request,
 	                      HttpServletResponse response){
-		this.fileService = new FileService(request,response);
+		fileService.response = response;
+		fileService.request = request;
 		fileService.run();
 	}
 	
 	@Override
 	protected void doDelete(HttpServletRequest request,
 	                        HttpServletResponse response) {
-		this.fileService = new FileService(request,response);
+		fileService.response = response;
+		fileService.request = request;
 		fileService.run();
 	}
 	
 	@Override
 	protected void doPut(HttpServletRequest request,
 	                        HttpServletResponse response) {
-		this.fileService = new FileService(request,response);
+		fileService.response = response;
+		fileService.request = request;
 		fileService.run();
 	}
 }
